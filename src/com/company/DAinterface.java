785fc1e970596e80fc606a0c2babbb73ad598441
Alejandra Casanova <alejandracasanova@Alejandras-MacBook-Pro.local>
2020-03-14 21:36:50 -0400

    dynamic array implementation

---
+++ src/com/company/DAinterface.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Iterator;
+
+public interface DAinterface<T> {
+    public int size();
+    public boolean isEmpty();
+    public T get(int index);
+    public void set(T elem, int index);
+    public void clear();
+    public void add(T elem);
+    public void removeAt(int index);
+    public boolean remove(T elem);
+    public int indexOf(T elem);
+    public boolean contains();
+    public Iterator<T> iterator();
+    public String toString();
+
+
+}
---
+++ src/com/company/dinamicArray.java
@@ -0,0 +1,107 @@
+package com.company;
+
+import java.util.Iterator;
+
+public class dinamicArray<T> implements DAinterface<T>, Iterable<T>{
+    public T[] array;
+    public int capacity; //actual size of array
+    public int size; //size the user defines the array
+
+    public dinamicArray(int cap){
+      if( cap<0) throw new IllegalArgumentException("Illegal capacity: "+cap);
+      this.capacity = cap;
+      array = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+
+        return size() == 0;
+    }
+
+    @Override
+    public T get(int index) {
+        if(index<0 || index>=size ) throw new IndexOutOfBoundsException(" Index is out of bounds");
+
+        return array[index];
+    }
+
+    @Override
+    public void set(T elem, int index) {
+        if(index<0 || index>size) throw new IndexOutOfBoundsException("Index out of bounds");
+        else array[index]=elem;
+    }
+
+    @Override
+    public void clear() {
+        for(int i = 0; i < size; i++){
+            array[i]=null;
+        }
+
+    }
+
+    @Override
+    public void add(T elem) {
+        /*In this method we are going to resize the array. First we verify if by adding one we reached the
+        * maximum capacity. If yes then we double the capacity, create a new array of the new capacity and
+        * add all the elements on the array to our new array.
+        * If we have not reached the end then we just add the element to the current array*/
+
+        if(size + 1>= capacity) {
+            capacity = capacity * 2;
+            T[] arr = (T[]) new Object[capacity];
+            for(int i = 0; i <size; i++) arr[i]=array[i];
+            array = arr;
+        }
+        array[size++]=elem;
+    }
+
+    @Override
+    public void removeAt(int index) {
+        /*In this method we will remove the element if found on the array and maintain the array on the same order
+        *as it was before*/
+        if(index <0 || index >=size) throw new IndexOutOfBoundsException("Index out of bounds");
+        T elem = array[index];
+        T[] new_arr = (T[]) new Object[capacity-1];
+        for(int i = 0; i<size; i++){
+            if(array[i] == elem) continue;
+            else new_arr[i]= array[i];
+        }
+        array=new_arr;
+        capacity=capacity-1;
+    }
+
+    @Override
+    public boolean remove(T elem) {
+        /*In this method we are going to remove the element if it is found on
+        *the array*/
+        int index = indexOf(elem);
+        if(index == -1) return false;
+        removeAt(index);
+        return true;
+    }
+
+    @Override
+    public int indexOf(T elem) {
+        int result = -1;
+        for(int i= 0; i<size; i++){
+            if(array[i]==elem)result= i;
+        }
+        return result;
+    }
+
+    @Override
+    public boolean contains() {
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
