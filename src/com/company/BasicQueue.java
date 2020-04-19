b86a4a986f90af0d98d9b070416f97a90ff0e37a
alejandra <alejandra.casanova@upr.edu>
2020-04-19 19:36:19 -0400

    added methods for queue' ;

--- src/com/company/BasicQueue.java
+++ src/com/company/BasicQueue.java
@@ -41,5 +41,36 @@ public class BasicQueue<E> {
         }
         return ans;
     }
+    public boolean contains(E item){
+        boolean ans = false;
+        if((size() == 0))ans = false;
+        else{
 
+            for(int i = start; i< end; i++){
+                if(data[i].equals(item)) ans = true;
+            }
+        }
+        return ans;
+    }
+    public E accesstem(E item){
+        E ans = null;
+        if(size() == 0) throw new IllegalArgumentException("The queue is empty");
+        else{
+            E curr = null;
+            for(int i = 0; i< end; i++){
+                curr = deQueue();
+                if(curr.equals(data[i])) ans = curr;
+            }
+        }
+        return ans;
+    }
+    public E access (int position){
+        if(position>end || size() == 0) throw new IllegalArgumentException("The position is not valid.");
+        E elem = null;
+        int trueIndex = 0;
+        for(int i = start; i<end; i++){
+            if(trueIndex == position) return data[i];
+        }trueIndex++;
+        throw new IllegalArgumentException("no position found");
+    }
 }
