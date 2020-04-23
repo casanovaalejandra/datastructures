8b35ebcaf964a27621610e5853121a1345862917
alejandra <alejandra.casanova@upr.edu>
2020-04-22 21:12:28 -0400

    added method to add and remove node to the linkedlist

--- src/com/company/BasicLinkedList.java
+++ src/com/company/BasicLinkedList.java
@@ -3,11 +3,36 @@ package com.company;
 public class BasicLinkedList<E> {
     private Node first;
     private Node last;
+    private int nodeCount;
     public BasicLinkedList() {
         this.first = null;
         this.last = null;
+        nodeCount = 0;
     }
+    public int size(){
+        return this.nodeCount;
+    }
+    public void add (E item){
+        if(first == null){
+            first = new Node(item);
+            last = first;
+        }else{
+            Node n = new Node(item);
+            last.setNextNode(n);
+            last = n;
+        }
+    nodeCount++;
 
+    }
+    public E remove() {
+        if (first == null) {
+            throw new IllegalArgumentException("The list is empty");
+        }
+        E curr = this.first.getNodeItem();
+        first = first.getNextNode();
+        nodeCount--;
+        return curr;
+    }
     private class Node {
         private Node nextNode;
         private E nodeVal;
