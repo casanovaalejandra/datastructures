890572f38968f1965af2c52c62935ce1f9368b9f
alejandra <alejandra.casanova@upr.edu>
2020-08-02 21:21:35 -0400

    old work + insertion sort alg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        generalAlgorithms alg = new generalAlgorithms();
+        int[] arr = {2,8,5,6,0,1};
+        List<Integer> sortedArr = alg.insertionSort(arr);
+        System.out.println(sortedArr);
     }
 
 }
--- src/com/company/generalAlgorithms.java
+++ src/com/company/generalAlgorithms.java
@@ -2,13 +2,26 @@ package com.company;
 import java.util.*;
 import java.util.List;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class generalAlgorithms {
-    public List<String> generatePermutations(String str){
+    public List<String> generatePermutations(String str) {
         return null;
     }
     public List<String> generateCombinations(String str){
         return null;
     }
-
+    public List<Integer> insertionSort(int[] arr) {
+        if( arr.length == 0 || arr.length == 1) return Arrays.stream(arr).boxed().collect(Collectors.toList());
+        for(int i = 1; i < arr.length; i++){
+            Integer value = arr[i];
+            int hole = i;
+            while(hole>0 && arr[hole-1]>value){
+                arr[hole] = arr[hole-1];
+                hole--;
+            }
+            arr[hole] = value;
+        }
+        return Arrays.stream(arr).boxed().collect(Collectors.toList());
+    }
 }
